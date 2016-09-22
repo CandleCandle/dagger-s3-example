a0c474df057369aa9b49ec6852be7d7ca3ca438b
Candle <candle@candle.me.uk>
2016-09-22 16:18:56 +0100

    Added an argument to test how passing of CLI args can work.

--- src/main/java/uk/me/candle/dagger/example/Main.java
+++ src/main/java/uk/me/candle/dagger/example/Main.java
@@ -4,19 +4,36 @@ import com.amazonaws.ClientConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import dagger.Component;
+import dagger.Lazy;
 import dagger.Module;
 import dagger.Provides;
 import java.net.MalformedURLException;
 import java.net.URL;
 import javax.inject.Inject;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
 public class Main {
 
-    public static void main(String[] args) {
-        MainComponent t = DaggerMainComponent
-                .create();
-        t.s3Lister().run();
+    public static void main(String[] rawArgs) {
+
+        final Args args = new Args();
+        final CmdLineParser clp = new CmdLineParser(args);
+
+        try {
+            clp.parseArgument(rawArgs);
+            MainComponent t = DaggerMainComponent.create();
+            t.s3Lister().run(args);
+
+        } catch (final CmdLineException e) {
+            System.err.println(e.getMessage());
+            clp.printSingleLineUsage(System.err);
+            System.err.println();
+            clp.printUsage(System.err);
+        }
     }
 }
 
@@ -25,24 +42,42 @@ interface MainComponent {
     S3Lister s3Lister();
 }
 
-class S3Lister implements Runnable {
+class Args {
+    @Option(name = "--bucket", aliases = {"-b", "-B"}, metaVar = "<BUCKET>", usage = "some bucket") private String bucket;
+
+    public String getBucket() { return bucket; }
+    public boolean hasBucket() { return bucket != null; }
+}
+
+class S3Lister {
     private final AmazonS3 s3Client;
-    private final BucketPrinter bucketPrinter;
+    private final Lazy<BucketPrinter> bucketPrinter;
+    private final Lazy<S3ObjectPrinter> objectPrinter;
 
     @Inject
-    public S3Lister(AmazonS3 s3Client, BucketPrinter bucketPrinter) {
+    public S3Lister(AmazonS3 s3Client, Lazy<BucketPrinter> bucketPrinter, Lazy<S3ObjectPrinter> objectPrinter) {
         this.s3Client = s3Client;
         this.bucketPrinter = bucketPrinter;
+        this.objectPrinter = objectPrinter;
     }
 
-    @Override
-    public void run() {
-        for (Bucket b : s3Client.listBuckets()) {
-            bucketPrinter.printBucket(b);
+    public void run(Args args) {
+        if (args.hasBucket()) {
+            s3Client.listObjects(args.getBucket()).getObjectSummaries()
+                    .forEach(o -> objectPrinter.get().printBucket(o));
+        } else {
+            s3Client.listBuckets().forEach(b -> bucketPrinter.get().printBucket(b));
         }
     }
 }
 
+class S3ObjectPrinter {
+    @Inject public S3ObjectPrinter() { }
+
+    void printBucket(S3ObjectSummary o) {
+        System.out.println(String.format("s3://%s/%s", o.getBucketName(), o.getKey()));
+    }
+}
 class BucketPrinter {
     @Inject public BucketPrinter() { }
 
